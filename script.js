63792d11cd92c297fa97001a4e5ca1637c3faaf6
tyVespA <migliavacca.marco08@gmail.com>
2022-12-17 17:27:03 +0100

    Add hover effects, remove mouseenter reading notifications

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ const markAllAsReadBtn = document.querySelector(".mark-all-as-read");
 
 const notificationCount = document.querySelector(".notification-count");
 
-
-
-unreadNotifications.forEach((unreadNotification) => {
-  unreadNotification.addEventListener("mouseenter", () => {
-  unreadNotification.classList.remove("unread");
-  if (notificationCount.textContent > 0) {
-    notificationCount.textContent -= 1;
-  }
-  })
-})
+// unreadNotifications.forEach((unreadNotification) => {
+//   unreadNotification.addEventListener("mouseenter", () => {
+//   unreadNotification.classList.remove("unread");
+//   if (notificationCount.textContent > 0) {
+//     notificationCount.textContent -= 1;
+//   }
+//   })
+// })
 
 markAllAsReadBtn.addEventListener("click", () => {
   unreadNotifications.forEach((unreadNotification) => {
